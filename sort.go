62b2b4b79c792374ddaa3220bef08c1511fc11d2
Pavel <freepk@Pavels-MacBook-Pro.local>
2021-03-08 02:36:04 +0300

    nice

--- sort.go
+++ sort.go
@@ -1,36 +1,39 @@
 package main
 
 func heapSort(a []int) {
-	n := len(a)
-	i := (n - 1) / 2
-	for i >= 0 {
-		siftDown(a, i, n)
-		i--
+	right := len(a)
+	if right < 2 {
+		return
 	}
-	i = (n - 1)
-	for i > 0 {
-		a[0], a[i] = a[i], a[0]
-		siftDown(a, 0, i)
-		i--
+	left := (right - 2) / 2
+	for left >= 0 {
+		siftDown(a, left, right)
+		left--
+	}
+	right--
+	for right > 0 {
+		a[0], a[right] = a[right], a[0]
+		siftDown(a, 0, right)
+		right--
 	}
 }
 
-func siftDown(a []int, i, n int) {
-	j := (i * 2) + 1
-	k := j + 1
-	for j < n {
-		if k < n {
-			if a[j] < a[k] {
-				j++
-				k++
+func siftDown(a []int, left, right int) {
+	curr := (left * 2) + 1
+	next := curr + 1
+	for curr < right {
+		if next < right {
+			if a[curr] < a[next] {
+				curr++
+				next++
 			}
 		}
-		if a[i] >= a[j] {
-			break
+		if a[left] >= a[curr] {
+			return
 		}
-		a[i], a[j] = a[j], a[i]
-		i = j
-		j = (i * 2) + 1
-		k = j + 1
+		a[left], a[curr] = a[curr], a[left]
+		left = curr
+		curr = (left * 2) + 1
+		next = curr + 1
 	}
 }
