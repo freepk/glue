1e675ee247e6745306e62e2546e283112c199559
Pavel <freepk@Pavels-MacBook-Pro.local>
2021-03-07 18:08:47 +0300

    fix

--- main.go
+++ main.go
@@ -1,37 +1,32 @@
 package main
 
 import (
-	//"fmt"
 	"github.com/valyala/fasthttp"
 )
 
 const (
-	argName     = `nm`
-	servicePort = `:8080`
+	argumentName = `nm`
+	servicePort  = `:8080`
 )
 
 func serviceHandler(ctx *fasthttp.RequestCtx) {
 	args := ctx.QueryArgs()
-	buf := args.Peek(argName)
-	bufLen := len(buf)
-	if bufLen == 0 {
+	buff := args.Peek(argumentName)
+	buffLen := len(buff)
+	if buffLen == 0 {
 		return
 	}
 	items := make([]int, 0, 256)
-	items, scanLen := scanNums(items, buf)
-	if scanLen != bufLen {
+	items, scanLen := scanNums(items, buff)
+	if scanLen != buffLen {
 		return
 	}
-	//fmt.Println("args", string(buf))
 	heapSort(items)
-	items = dedupNums(items)
-	//fmt.Println("items", items)
-	shards := make([]int, len(items))
-	for i, item := range items {
-		shards[i] = item / 1000000
+	shards := make([]int, 0, 256)
+	for i := 0; i < len(items); i++ {
+		shards = append(shards, (items[i] / 1000000))
 	}
 	shards = dedupNums(shards)
-	//fmt.Println("shards", shards)
 }
 
 func main() {
