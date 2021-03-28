878477fbad042fb49dce25ee17090e9f40e19ab8
freepk <freepk@mail.ru>
2021-03-28 22:53:39 +0300

    fix

--- worker.go
+++ worker.go
@@ -1,34 +1,41 @@
 package main
 
+import (
+	"github.com/valyala/fasthttp"
+)
+
 type worker struct {
 	pool *workerPool
 	join chan []byte
+	data [][]byte
 }
 
 func newWorker(p *workerPool) *worker {
 	w := new(worker)
 	w.pool = p
 	w.join = make(chan []byte)
+	w.data = make([][]byte, 64)
 	p.workers <- w
 	return w
 }
 
 func makeHttpRequest(buf []byte, url string) []byte {
+	_, buf, _ = fasthttp.Get(buf, url)
 	return buf
 }
 
-func makeHttpRequestAsync(buf []byte, url string, out chan<- []byte) {
-	out <- makeHttpRequest(buf, url)
+func makeHttpRequestAsync(buf []byte, url string, join chan<- []byte) {
+	join <- makeHttpRequest(buf, url)
 }
 
 func (w *worker) doAsync(buf []byte, urls []string) []byte {
 	n := len(urls)
 	for i := 0; i < n; i++ {
-		go makeHttpRequestAsync(nil, urls[i], w.join)
+		go makeHttpRequestAsync(w.data[i][:0], urls[i], w.join)
 	}
 	for i := 0; i < n; i++ {
-		body := <-w.join
-		buf = append(buf, body...)
+		w.data[i] = <-w.join
+		buf = append(buf, w.data[i]...)
 	}
 	return buf
 }
@@ -36,8 +43,7 @@ func (w *worker) doAsync(buf []byte, urls []string) []byte {
 func (w *worker) doSync(buf []byte, urls []string) []byte {
 	n := len(urls)
 	for i := 0; i < n; i++ {
-		body := makeHttpRequest(nil, urls[i])
-		buf = append(buf, body...)
+		buf = makeHttpRequest(buf, urls[i])
 	}
 	return buf
 }
