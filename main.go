fe5540b4e0d158ad5f0c4de2e603a190644920a9
freepk <freepk@mail.ru>
2021-04-02 15:27:19 +0300

    Refactor

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/valyala/fasthttp"
 	"log"
+	"sync"
 )
 
 const (
@@ -20,34 +21,75 @@ const (
 	remoteProductsArg = `product`
 )
 
-type service struct {
-	shards []string
+type productResult struct {
+	wait     *sync.WaitGroup
+	products []int
 }
 
-func newService() *service {
-	return new(service)
+func newProductResult() *productResult {
+	return &productResult{
+		wait: &sync.WaitGroup{},
+	}
 }
 
-func (svc *service) handleProducts(ctx *fasthttp.RequestCtx, remotePath string) {
-	products, _ := parseUints(make([]int, 0, 1024), ctx.QueryArgs().Peek(productsArg))
-	heapSort(products)
-	products = dedupInts(products)
+func (pr *productResult) setProductBytes(buf []byte) {
+	pr.products, _ = parseUints(pr.products[:0], buf)
+	heapSort(pr.products)
+	pr.products = dedupInts(pr.products)
+}
 
-	args := ctx.QueryArgs()
-	args.Del(productsArg)
+func (pr *productResult) requestShardProducts(shard int, products []int) {
+	defer pr.wait.Done()
+	// fmt.Println(shard, products)
+}
 
+func (pr *productResult) requestProducts() {
+	n := len(pr.products)
 	i := 0
 	j := 0
-	for i < len(products) {
+	for i < n {
 		j = i
-		for j < len(products) {
-			if (products[i] / 1000000) != (products[j] / 1000000) {
+		shard := productShard(pr.products[i])
+		for j < n {
+			if shard != productShard(pr.products[j]) {
 				break
 			}
 			j++
 		}
+		pr.wait.Add(1)
+		go pr.requestShardProducts(shard, pr.products[i:j])
 		i = j
 	}
+	pr.wait.Wait()
+}
+
+type productService struct {
+	shardURLs  []string
+	resultPool *sync.Pool
+}
+
+func newService() *productService {
+	return &productService{
+		resultPool: &sync.Pool{
+			New: func() interface{} {
+				return newProductResult()
+			},
+		},
+	}
+}
+
+func productShard(i int) int {
+	return i / 1000000
+}
+
+func (svc *productService) handleProducts(ctx *fasthttp.RequestCtx, remotePath string) {
+
+	result := svc.resultPool.Get().(*productResult)
+	defer svc.resultPool.Put(result)
+
+	result.setProductBytes(ctx.QueryArgs().Peek(productsArg))
+	result.requestProducts()
+
 }
 
 func main() {
