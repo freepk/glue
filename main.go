cf69bf602c244751947e3cf5a287f3fbab08da31
freepk <freepk@mail.ru>
2021-04-02 14:16:43 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/valyala/fasthttp"
 	"log"
-	"unsafe"
 )
 
 const (
@@ -12,97 +11,62 @@ const (
 	filtersPath = `/v2/filters/only`
 	metricsPath = `/metrics`
 	statePath   = `/state`
-
 	productsArg = `nm`
-	productsMax = 1024
+)
 
+const (
 	remoteCatalogPath = `/catalog`
 	remoteBasketPath  = `/basket`
 	remoteProductsArg = `product`
-
-	remoteService = `catalog-backend-part`
-	remoteDomain  = `wbx-ru.svc.k8s.dataline`
 )
 
-func partNum(i int) int {
-	return i / 1000000
+type service struct {
+	shards []string
 }
 
-func buildRemoteUrl(buf []byte, remotePath string, products []int) []byte {
-	buf = append(buf, `http://`...)
-	buf = append(buf, remoteService...)
-	buf = appendUint(buf, partNum(products[0]))
-	buf = append(buf, dotChar)
-	buf = append(buf, remoteDomain...)
-	buf = append(buf, remotePath...)
-	buf = append(buf, questMarkChar)
-	buf = append(buf, `locale=ru&lang=ru&`...)
-	buf = append(buf, remoteProductsArg...)
-	buf = append(buf, equalSignChar)
-	for i := 0; i < len(products); i++ {
-		buf = appendUint(buf, products[i])
-		buf = append(buf, semicolonChar)
-	}
-	return buf
+func newService() *service {
+	return new(service)
 }
 
-func asyncCallback(output, input []byte) []byte {
-	var err error
-	str := *(*string)(unsafe.Pointer(&input))
-	if _, output, err = fasthttp.Get(output, str); err != nil {
-		log.Print(str, err)
-	}
-	return output
-}
-
-var resultsPool = newLimitedPool(func() interface{} {
-	return newAsyncResult(asyncCallback)
-}, 128)
-
-func handleProducts(ctx *fasthttp.RequestCtx, remotePath string) {
-	args := ctx.QueryArgs()
-	products := make([]int, 0, productsMax)
-	products, _ = parseUints(products, args.Peek(productsArg))
+func (svc *service) handleProducts(ctx *fasthttp.RequestCtx, remotePath string) {
+	products, _ := parseUints(make([]int, 0, 1024), ctx.QueryArgs().Peek(productsArg))
 	heapSort(products)
 	products = dedupInts(products)
-	result := resultsPool.get().(*asyncResult)
-	result.resetTasks()
-	defer resultsPool.put(result)
-	n := len(products)
+
+	args := ctx.QueryArgs()
+	args.Del(productsArg)
+
 	i := 0
-	for i < n {
-		p := partNum(products[i])
-		j := i
-		for j < n {
-			if partNum(products[j]) != p {
+	j := 0
+	for i < len(products) {
+		j = i
+		for j < len(products) {
+			if (products[i] / 1000000) != (products[j] / 1000000) {
 				break
 			}
 			j++
 		}
-		task := result.newTask()
-		task.appendInput(buildRemoteUrl(task.resetInput(), remotePath, products[i:j]))
 		i = j
-	}
-	body := ctx.Response.Body()
-	body = result.await(body)
-	ctx.SetBody(body)
-}
-
-func serviceHandler(ctx *fasthttp.RequestCtx) {
-	if ctx.IsGet() {
-		switch string(ctx.Path()) {
-		case catalogPath:
-			handleProducts(ctx, remoteCatalogPath)
-		case basketPath:
-			handleProducts(ctx, remoteBasketPath)
-		case filtersPath:
-		case metricsPath:
-		case statePath:
-		}
 	}
 }
 
 func main() {
-	serviceAddr := `:8080`
-	fasthttp.ListenAndServe(serviceAddr, serviceHandler)
+	svc := newService()
+	handler := func(ctx *fasthttp.RequestCtx) {
+		if ctx.IsGet() {
+			switch string(ctx.Path()) {
+			case catalogPath, remoteCatalogPath:
+				svc.handleProducts(ctx, remoteCatalogPath)
+			case basketPath, remoteBasketPath:
+				svc.handleProducts(ctx, remoteBasketPath)
+			case filtersPath:
+			case metricsPath:
+			case statePath:
+			}
+		}
+	}
+	addr := `:8080`
+	if err := fasthttp.ListenAndServe(addr, handler); err != nil {
+		log.Fatal(err)
+	}
 }
